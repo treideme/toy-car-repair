5d5bcb5e256795ce80b09f538e98cca496720400
Thomas Reidemeister <treideme@gmail.com>
2020-08-01 00:13:51 -0400

    Resolved #5

--- firmware/Src/app.c
+++ firmware/Src/app.c
@@ -3,6 +3,8 @@
  * @author Thomas Reidemeister
  * @file app.c
  */
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 #include "dac.h"
 #include "fatfs.h"
@@ -10,12 +12,21 @@
 #include "gpio.h"
 
 extern void SystemClock_Config(void); // Not exported by CubeMX
+extern void initialise_monitor_handles(void); // Enable Semihosting
+char semihosting_stdout_buf[16];
+char semihosting_stderr_buf[16];
 
 /**
  * @brief  The application entry point.
  * @retval int
  */
 int main(void) {
+  //Disable STDOUT buffering. Otherwise nothing will be printed before
+  //a newline character or when the buffer is flushed. This MUST be done
+  //before any writes to STDOUT to have any effect...
+  setvbuf(stdout, semihosting_stdout_buf, _IOLBF, sizeof(semihosting_stdout_buf));
+  setvbuf(stderr, semihosting_stderr_buf, _IOLBF, sizeof(semihosting_stderr_buf));
+
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
@@ -30,6 +41,8 @@ int main(void) {
 
   /* Infinite loop */
   for (;;) {
+    printf("Hello world\r\n");
+    HAL_Delay(1000);
   }
 
 }
